9745a820ac6b4dda0c1f6eef49c531705ff94a6f
Phelps17 <Phelps3@wisc.edu>
2016-06-13 22:41:17 -0500

    shuffling algorithm

---
+++ src/SortingIntro.java
@@ -0,0 +1,17 @@
+
+public class SortingIntro {
+	public static void main(String args[]) {
+		int[] iArray = {18, 2, 53, 4, 5, 14, 2, 54};
+		
+		for (int i = 0; i < iArray.length; i++) {
+			System.out.println(i);
+		}
+		
+		String[] sArray = {"hello", "world", "qwerty", "asdf"};
+		
+		for (int i = 0; i < sArray.length; i++) {
+			System.out.println(i);
+		}
+		
+	}
+}
