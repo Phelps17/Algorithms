eec895a512bb660e1531c962f169ac98446d6c62
Phelps17 <Phelps3@wisc.edu>
2016-07-05 23:22:30 -0500

    start of quicksort

--- Sorting Algorithms/src/QuickSort.java
+++ Sorting Algorithms/src/QuickSort.java
@@ -1,4 +1,53 @@
 
 public class QuickSort {
-
+	private QuickSort() {}
+	
+	public static void sort(int[] array) {
+		//random shuffle array
+		sort(array, 0, array.length-1);
+	}
+	
+	//quicksort from high to low
+	private static void sort(int[] array, int low, int high) {
+		if (high <= low) return;
+		
+		int i = partition(array, low, high);
+		sort(array, low, i-1);
+		sort(array, i, high);
+	}
+	
+	private static int partition(int[] array, int low, int high) {
+		int i = low;
+		int j = high + 1;
+		int v = array[low];
+		
+		while (true) {
+			//find item on low to swap
+			while (array[++i] < v) {
+				if (i == high) {
+					break;
+				}
+			}
+			
+			//find item on high to swap
+			while (v < array[--j]) {
+				if (j == low) {
+					break;
+				}
+			}
+			
+			//if pointers cross
+			if (i >= j) {
+				break;
+			}
+			
+			exchange(array, i, j);
+		}
+		
+		//put partitioning item v at array[j]
+		exchange(array, low, j);
+		
+		//now array[low ...j-1] <= array[j] <= a[j+1 ... high]
+		return j;
+	}
 }
