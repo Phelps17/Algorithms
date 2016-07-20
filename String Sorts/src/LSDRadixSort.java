4d0c21f838a4998e016e308ab4b5ffd62c81b9b0
Phelps17 <Phelps3@wisc.edu>
2016-07-19 23:07:05 -0500

    lsd radix format updates

--- String Sorts/src/LSDRadixSort.java
+++ String Sorts/src/LSDRadixSort.java
@@ -1,25 +1,27 @@
 
 public class LSDRadixSort {
-	public static void sort(String[] a, int W) {
-		int R = 256;
-		int N = a.length;
-		String[] aux = new String[N];
+	public static final int ASCII_SYMBOLS = 256;
+	
+	public static void sort(String[] array, int stringLengths) {
+		String[] auxArray = new String[array.length];
 		
-		//sort digits from right t left
-		for (int d = W-1; d >= 0; d--) {
+		//sort looking at digits from right to left
+		//LSD to MSD
+		for (int d = stringLengths-1; d >= 0; d--) {
 			//key index counting
-			int[] count = new int[R+1];
-			for (int i = 0; i < N; i++) {
-				count[a[i].charAt(d) + 1]++;
+			int[] count = new int[ASCII_SYMBOLS+1];
+			
+			for (int i = 0; i < array.length; i++) {
+				count[array[i].charAt(d) + 1]++;
 			}
-			for (int r = 0; r < R; r++) {
+			for (int r = 0; r < ASCII_SYMBOLS; r++) {
 				count[r+1] += count[r];
 			}
-			for (int i = 0; i < N; i++) {
-				aux[count[a[i].charAt(d)]++] = a[i];
+			for (int i = 0; i < array.length; i++) {
+				auxArray[count[array[i].charAt(d)]++] = array[i];
 			}
-			for (int i = 0; i < N; i++) {
-				a[i] = aux[i];
+			for (int i = 0; i < array.length; i++) {
+				array[i] = auxArray[i];
 			}
 		}
 	}
