15a958fe57789a07732395ab6a6db7ee4e73b10f
Phelps17 <Phelps3@wisc.edu>
2016-07-27 01:19:55 -0500

    brute force search

--- Substring Search/src/BruteForce.java
+++ Substring Search/src/BruteForce.java
@@ -1,9 +1,9 @@
 
 public class BruteForce {
-	//return the start indeex of the word or n if no matches
+	//return the start index of the word or n if no matches
 	public static int search(String pattern, String textToSearch) {
 		int m = pattern.length();
-		int n = pattern.length();
+		int n = textToSearch.length();
 		
 		for (int i = 0; i <= n-m; i++) {
 			int j;
--- Substring Search/src/SubstringSearch.java
+++ Substring Search/src/SubstringSearch.java
@@ -6,8 +6,37 @@ public class SubstringSearch {
 			+ "Where civil blood makes civil hands unclean.\n"
 			+ "From forth the fatal loins of these two foes\n"
 			+ "A pair of star-cross'd lovers take their life;\n";
+	final static String pattern1 = "blood";
+	final static String pattern2 = "f star-cro";
+	final static String pattern3 = "tears";
+	final static String pattern4 = "mutiny";
 	
 	public static void main(String[] args) {
-		System.out.println(BruteForce.search("blood", fullText));
+		int bruteIndex1 = BruteForce.search(pattern1, fullText);
+		checkSearchResult(fullText, pattern1, bruteIndex1);
+		
+		int bruteIndex2 = BruteForce.search(pattern2, fullText);
+		checkSearchResult(fullText, pattern2, bruteIndex2);
+		
+		int bruteIndex3 = BruteForce.search(pattern3, fullText);
+		checkSearchResult(fullText, pattern3, bruteIndex3);
+		
+		int bruteIndex4 = BruteForce.search(pattern4, fullText);
+		checkSearchResult(fullText, pattern4, bruteIndex4);
+	}
+	
+	public static void checkSearchResult(String fullText, String pattern, int index) {
+		if (index >= fullText.length()) {
+			System.out.println("Pattern not found in text.");
+			return;
+		}
+		
+		System.out.print("Looking for '" + pattern + "' and found '");
+		
+		for (int i = 0; i < pattern.length(); i++) {
+			System.out.print(fullText.charAt(index+i));
+		}
+		
+		System.out.print("'.\n");
 	}
 }
